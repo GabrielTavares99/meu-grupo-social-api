a8714aefbf020712ff73b30a93c89b3ad0cedcd0
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-07-17 23:25:29 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/com/mygroup/mygroup/MygroupApplication.java
@@ -0,0 +1,12 @@
+package com.mygroup.mygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygroupApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mygroup/mygroup/MygroupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mygroup.mygroup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MygroupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
